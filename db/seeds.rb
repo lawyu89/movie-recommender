354d1a381202253f2185bf6975f6a2b20c297a8a
Lawrence Yu <lawrenceyu1@gmail.com>
2015-07-19 14:22:10 -0700

    Add faker gem and working on seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#Generate Genres
+action = Genre.create(name: "Action & Adventure")
+comdey = Genre.create(name: "Comedy")
+scifi = Genre.create(name: "Science Fiction")
+horror = Genre.create(name: "Horror")
+mystery = Genre.create(name: "Mystery")
+romance = Genre.create(name: "Romance")
+family = Genre.create(name: "Family")
+drama = Genre.create(name: "Drama")
+thriller = Genre.create(name: "Thriller")
 
-#Generate
-  question = Question.create(title: ""
-    answer = Answer.create(content: )
-end
+#Generate Question Objects
+q1 = Question.create(title: "Pick an Animal!"
+q1_a1 = Answer.create(content: "Lion")
+q1_a1
+q1_a2 = Answer.create(content: "Dog")
+q1_a3 = Answer.create(content: "Dolphin")
+q1_a4 = Answer.create(content: "Lion")
