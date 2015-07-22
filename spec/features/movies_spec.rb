9b4cfd89e6fb5750fc43283ff9a30ff5c1047508
Lawrence Yu <lawrenceyu1@gmail.com>
2015-07-21 19:59:59 -0700

    Add a few more feature tests

--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -11,6 +11,8 @@ RSpec.feature "Movies", type: :feature do
       test1_genre.movies << movie1
       movie2 = Movie.create(name: "Movie2", rating: 8.0, released_year: 2010, movie_length: 4000, thumbnail_url: Faker::Avatar.image)
       test2_genre.movies << movie2
+      movie3 = Movie.create(name: "Movie3", rating: 3.8, released_year: 2009, movie_length: 5000, thumbnail_url: Faker::Avatar.image)
+      test1_genre.movies << movie3
       @questions = Question.create(title: "Test Question")
       Answer.create(content: "test1", question_id: @questions.id, genre_id:test1_genre.id)
       Answer.create(content: "test2", question_id: @questions.id, genre_id:test2_genre.id)
@@ -39,13 +41,29 @@ RSpec.feature "Movies", type: :feature do
         click_button 'See Results'
         expect(page).to have_content('Movie1')
       end
-      scenario "show movie related to genre picked" do
+      scenario "show more information after clicking on image" do
         visit '/index'
         click_button 'Start Personality Quiz'
         find(:xpath, '//input[following-sibling::text()[1][normalize-space(.) = "test1"]]').set(true)
         click_button 'See Results'
         find('.front').click
         expect(page).to have_content('test1_genre')
+      end
+      scenario "do not show movie with unrelated genre" do
+        visit '/index'
+        click_button 'Start Personality Quiz'
+        find(:xpath, '//input[following-sibling::text()[1][normalize-space(.) = "test1"]]').set(true)
+        click_button 'See Results'
+        find('.front').click
+        expect(page).to have_no_content('test2_genre')
+      end
+      scenario "do not show movie's with ratings less than 4" do
+        visit '/index'
+        click_button 'Start Personality Quiz'
+        find(:xpath, '//input[following-sibling::text()[1][normalize-space(.) = "test1"]]').set(true)
+        click_button 'See Results'
+        find('.front').click
+        expect(page).to have_no_content('Movie3')
       end
     end
   end
